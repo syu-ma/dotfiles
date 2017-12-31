5af8f90ba94dab16695fc2fc995e31a5bf3d6040
Shingo Masuda <sin.five.m@gmail.com>
2017-12-31 16:56:35 +0900

    Add "pause" and some fix

--- mklink.bat
+++ mklink.bat
@@ -3,4 +3,5 @@ mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
 mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
 mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
 mklink /D %HOMEPATH%"\.vim\bundle" %HOMEPATH%"\dotfiles\bundle"
+pause
 exit 0
