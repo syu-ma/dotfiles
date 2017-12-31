4d947f3045acdde71ca062cf56408122d43905de
Shingo Masuda <sin.five.m@gmail.com>
2017-12-31 16:39:22 +0900

    Create

---
+++ mklink.bat
@@ -0,0 +1,6 @@
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
+mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
+mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
+mklink /D %HOMEPATH%"\.vim\bundle" %HOMEPATH%"\dotfiles\bundle"
+exit 0
