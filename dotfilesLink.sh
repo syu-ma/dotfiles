1479b479401c4bd23a090a24db1b8b7a4878eaa1
Shingo Masuda <sin.five.m@gmail.com>
2018-08-11 19:44:48 +0900

    Fix symbolic link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
+mkdir ~/.vim
+ln -sf ~/dotfiles/colors ~/.vim/
 #ln -sf ~/dotfiles/ftdetect ~/.vim
 #ln -sf ~/dotfiles/indent ~/.vim
 #ln -sf ~/dotfiles/bundle ~/.vim
